1efbda3e4473a0e1fa29f1fc5577dab0d259e78f
zhengrenkun <zhengrenkun@chandashi.com>
2023-08-17 15:16:18 +0800

    feat

--- server/src/config.ts
+++ server/src/config.ts
@@ -3,7 +3,8 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 const config = {
-  BEMFA_CLIENT_ID: process.env.BEMFA_CLIENT_ID
+  BEMFA_CLIENT_ID: process.env.BEMFA_CLIENT_ID,
+  SERVER_PORT: process.env.SERVER_PORT
 }
 
 export default config
--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,7 @@ import { Koast } from 'koast'
 import routers from './controller'
 import { openDb } from './utils/db'
 
+import config from './config'
 import './mqtt/broker'
 import './mqtt/bemfa_mqtt'
 
@@ -11,11 +12,11 @@ async function main() {
   const app = new Koast({ proxy: true })
 
   app.useSwagger(routers)
-  console.log('swagger address http://localhost:3000/swagger-ui/index.html')
+  console.log('swagger address http://localhost:' + config.SERVER_PORT + '/swagger-ui/index.html')
 
   app.useRouter(routers)
-  app.listen(3000, () => {
-    console.log('server start on http://localhost:3000')
+  app.listen(config.SERVER_PORT, () => {
+    console.log('server start on http://localhost:' + config.SERVER_PORT)
   })
 }
 
